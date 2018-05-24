03923a828b72cce9b6c8a659d9a24d660a458204
mrchupee <ahlaing@y7mail.com>
2018-05-24 14:56:27 +1000

    Implement dummy MartialArtsActivity

--- app/src/main/java/tk/mrchupee/www/hackinghelper/MainActivity.java
+++ app/src/main/java/tk/mrchupee/www/hackinghelper/MainActivity.java
@@ -1,7 +1,10 @@
 package tk.mrchupee.www.hackinghelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,15 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hacking program clustermess
+
+        // Martial Arts tables and notes
+        TextView martial_arts = (TextView) findViewById(R.id.martial_arts_button);
+        // Set a click listener on that View
+        martial_arts.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent ma_intent = new Intent(MainActivity.this, MartialArtsActivity.class);
+                startActivity(ma_intent);
+            }
+        });
+
+        // Booty L1 and L2 tables
+
+        // Metachemistry L1 and L2 tables
+
+        // Notebook
     }
 
-    // Hacking program clustermess
 
-    // Martial Arts tables and notes
-
-    // Booty L1 and L2 tables
-
-    // Metachemistry L1 and L2 tables
-
-    // Notebook
 }
---
+++ app/src/main/java/tk/mrchupee/www/hackinghelper/MartialArtsActivity.java
@@ -0,0 +1,12 @@
+package tk.mrchupee.www.hackinghelper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MartialArtsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
